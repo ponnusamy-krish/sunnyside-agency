79660879af45e6268fc6a72e9f16c644f0b24bfe
sam <ponnusamykrish23@gmail.com>
2022-07-14 21:36:48 +0530

    commit

--- app.js
+++ app.js
@@ -5,22 +5,12 @@ let ham = document.getElementById("hamenu")
 hampic.addEventListener("click", ()=>{
     if(ham.style.display == "none" ){
         
-        ham.style.display="block"
+        ham.style.display=="block"
     }
    
-<<<<<<< HEAD
    else if(ham.style.display == "block" ){
         ham.style.display = "none"
        
-=======
-    if(ham.style.visibility == "visible" || ham.style.display=="block"){
-        ham.style.visibility = "hidden"
-        ham.style.display ="none"
-    }
-    else if(ham.style.visibility == "hidden" || ham.style.display=="block"){
-        ham.style.visibility = "visible"
-        ham.style.display=="block"
->>>>>>> ecadc1756af60e2e7ad5d3a2297edb7b1ffb4425
     }
    
 })
